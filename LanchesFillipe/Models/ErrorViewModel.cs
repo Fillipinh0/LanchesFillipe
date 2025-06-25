954334295f3781d23c6ae4a8b6d5b8e43f68ce33
Fillipe <filliperesende3@gmail.com>
2025-06-25 19:23:15 -0300

    Add project files.

---
+++ LanchesFillipe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LanchesFillipe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
